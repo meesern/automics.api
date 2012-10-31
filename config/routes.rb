3add10b35010a5957065b7514ed3cf2f88732a05
Automics II <automicsii-admin@horizab2.miniserver.com>
2012-10-31 16:29:53 +0000

    initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,66 @@
 AutomicsApi::Application.routes.draw do
   root :to => 'front#index'
 
+  #Group
+  get  '/v1/group'  			=> 'group#index'
+  post '/v1/group'  			=> 'group#create'
+  get  '/v1/group/:name'  		=> 'group#show'
+  post '/v1/group/:name' 		=> 'group#update'
+  delete '/v1/group/:name' 		=> 'group#delete'
+
+  #User
+  get  '/v1/user'  			=> 'user#index'
+  post '/v1/user'  			=> 'user#create'
+  get  '/v1/user/:name'  		=> 'user#show'
+  post '/v1/user/:name' 		=> 'user#update'
+  delete '/v1/user/:name' 		=> 'user#delete'
+
+  #Organisation
+  get  '/v1/organisation'  		=> 'organisation#index'
+  get  '/v1/organisation/:name'  	=> 'organisation#show'
+
+  #Event
+  get  '/v1/organisation/:org/event'  	=> 'event#index'
+  get  '/v1/event/:id'                	=> 'event#show'
+
+  #Theme
+  get  '/v1/event/:event/theme'  	=> 'theme#index'
+  get  '/v1/theme/:id'  		=> 'theme#show'
+
+  #Resource
+  get  '/v1/resource/:id'  		=> 'resource#show'
+
+  #Photo
+  get  '/v1/photo'  			=> 'photo#index'
+  post '/v1/photo/' 			=> 'photo#create'
+  get  '/v1/photo/:id'  		=> 'photo#show'
+  post '/v1/photo/:id' 			=> 'photo#update'
+  delete '/v1/photo/:id' 		=> 'photo#delete'
+
+  #Annotation
+  get  '/v1/photo/:id/annotation'  	=> 'annotation#index'
+  post '/v1/photo/:id/annotation' 	=> 'annotation#create'
+  get  '/v1/annotation/:id'  		=> 'annotation#show'
+  post '/v1/annotation/:id' 		=> 'annotation#update'
+  delete '/v1/annotation/:id' 		=> 'annotation#delete'
+
+  #Comic
+  get  '/v1/comic'  			=> 'comic#index'
+  post '/v1/comic'			=> 'comic#create'
+  get  '/v1/comic/:id' 			=> 'comic#show'
+  post '/v1/comic/:id' 			=> 'comic#update'
+  delete '/v1/comic/:id' 		=> 'comic#delete'
+
+  #Panel
+  get  '/v1/comic/:id/panel'  		=> 'panel#index'
+  post '/v1/comic/:id/panel' 		=> 'panel#create'
+  get  '/v1/panel/:id'  		=> 'panel#show'
+  post '/v1/panel/:id' 			=> 'panel#update'
+  delete '/v1/panel/:id' 		=> 'panel#delete'
+
+
+  #get '/v1/dish/:dish/person/:person' => 'api#dish', :qualifier=>'person'
+
   match 'search' => 'front#search', :as => 'site_search'
 
   # The priority is based upon order of creation:
