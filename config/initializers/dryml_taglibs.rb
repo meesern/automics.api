28f6bf9e05b1192ed86f2bd68383be45224b1155
Automics II <automicsii-admin@horizab2.miniserver.com>
2012-12-04 11:19:16 +0000

    removed dryml precomile for thorny production theme problem

--- config/initializers/dryml_taglibs.rb
+++ config/initializers/dryml_taglibs.rb
@@ -1,1 +1,3 @@
-Dryml.precompile_taglibs if File.basename($0) != "rake" && Rails.env.production?
+#Comment out this line to fix the theme not updating properly bug (as seen by
+#clean theme with ajax spinners and search sidebar
+#Dryml.precompile_taglibs if File.basename($0) != "rake" && Rails.env.production?
