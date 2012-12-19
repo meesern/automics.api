8ec583efa46b5431392c38b7df8c902cf6ea35a7
Rupert Meese <rupert.meese@lightmind.co.uk>
2012-12-19 10:58:57 +0000

    fixed cross domain access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :allow_cross_domain_access, 
+                :only=>[:api_index, :api_show, :api_create, :api_update, :api_delete]
+
+
+  def allow_cross_domain_access
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "*"
+  end
+
+  def raise_404
+    raise ActionController::RoutingError.new('Not Found')
+  end
+
 end
